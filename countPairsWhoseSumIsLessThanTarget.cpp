313f3d593032ffe4828ba67e8bc85b1b66733e66
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2024-03-01 18:06:13 +0530

    LeetCode-2824

--- countPairsWhoseSumIsLessThanTarget.cpp
+++ countPairsWhoseSumIsLessThanTarget.cpp
@@ -13,3 +13,26 @@ public:
         return count;
     }
 };
+
+
+/*📌Using Binary Search */
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int i=0;
+        int j;
+
+        int count = 0;
+        while(i<nums.size()){
+            j=i+1;
+            while(j<nums.size()){
+                if(nums[i]+nums[j]<target){
+                    count++;
+                }
+                j++;
+            }
+            i++;
+        }
+        return count;
+    }
+};
