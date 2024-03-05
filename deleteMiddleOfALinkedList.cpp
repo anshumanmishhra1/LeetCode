f96712cd89475740dd5b038ef0269ed3741c0b79
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2024-03-05 13:18:33 +0530

    LeetCode-2095

--- deleteMiddleOfALinkedList.cpp
+++ deleteMiddleOfALinkedList.cpp
@@ -1,17 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-    
+        if (!head || !head->next) // If list is empty or has only one node
+            return nullptr;
+
         ListNode* fast = head;
         ListNode* slow = head;
-        ListNode* prev = NULL;
-        
-        while(fast != NULL and fast->next!=NULL){
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-        prev->next = slow->next;
+
+        if (prev) {
+            prev->next = slow->next;
+            delete slow;
+        } else {
+            head = head->next;
+            delete slow;
+        }
+
         return head;
     }
 };
